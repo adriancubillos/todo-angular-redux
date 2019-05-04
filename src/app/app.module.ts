993e8330a08defb18c1920c8821414521417412c
Adrian <adriancubillos@gmail.com>
2019-05-04 16:07:15 -0400

    redux devtools. refactor fat switch case

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { NgRedux, NgReduxModule, DevToolsExtension } from '@angular-redux/store';
 import { NgModule } from '@angular/core';
-
-import { NgRedux, NgReduxModule } from '@angular-redux/store';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { IAppState, INITIAL_STATE, rootReducer } from './store';
 import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
@@ -16,7 +15,11 @@ import { TodoListComponent } from './todo-list/todo-list.component';
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<IAppState>) {
-    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  constructor(ngRedux: NgRedux<IAppState>, devTools: DevToolsExtension) {
+    const storeEnhancers = devTools.isEnabled() // <- New
+      ? [ devTools.enhancer() ] // <- New
+      : []; // <- New
+
+    ngRedux.configureStore(rootReducer, INITIAL_STATE, [], storeEnhancers);
   }
 }
